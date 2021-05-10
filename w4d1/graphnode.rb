6c0efe07c8201297f487cf8977c886db03f6f77d
Priyesh Shah <priyesh.shah147@gmail.com>
2021-05-09 20:28:32 -0400

    Create GraphNode class and use bfs method using queue for neighbor node

---
+++ w4d1/graphnode.rb
@@ -0,0 +1,46 @@
+class GraphNode
+
+    attr_accessor :val, :neighbors
+
+    def initialize(val)
+        self.val = val
+        self.neighbors = []
+    end
+
+    def add_neighbor(node)
+        self.neighbors << node
+    end
+
+
+end
+
+def bfs(starting_node, target_value)
+    queue = [starting_node]
+    visited = Set.new()
+
+    while !queue.empty?
+        node = queue.shift
+        if !visited.include?(node)
+            return node.val if nod.val == target_value
+            visited.add(node)
+            queue += node.neighbors
+        end
+    end
+
+    return nil
+    
+end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
+p f
+p a
+
